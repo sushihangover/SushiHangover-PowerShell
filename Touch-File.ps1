fe6a0bb901d1645cc717dc2f9d358729c36cccbc
sushihangover <robertnees@gmail.com>
2012-10-14 17:25:21 -0700

    Added Touch-File.ps1
    touch-file -- change file access and modification times

---
+++ Touch-File.ps1
@@ -0,0 +1,72 @@
+<#
+    .NOTES
+        Copyright 2013 Robert Nees
+        Licensed under the Apache License, Version 2.0 (the "License");
+        http://sushihangover.blogspot.com
+    .SYNOPSIS
+        touch-file -- change file access and modification times
+    .DESCRIPTION
+     The touch utility sets the modification and access times of files.  If any file does not exist, 
+     it is created with default permissions. (see examples)
+    .EXAMPLE
+        TODO : Add Examples
+    .LINK
+        http://sushihangover.blogspot.com
+#>
+function Touch-File([string[]]$paths) {
+  begin {
+    function updateFileSystemInfo([System.IO.FileSystemInfo]$fsInfo) {
+      $datetime = get-date
+      $fsInfo.CreationTime = $datetime
+      $fsInfo.LastWriteTime = $datetime
+      $fsInfo.LastAccessTime = $datetime
+    }
+   
+    function touchExistingFile($arg) {
+      if ($arg -is [System.IO.FileSystemInfo]) {
+        updateFileSystemInfo($arg)
+      }
+      else {
+        $resolvedPaths = resolve-path $arg
+        foreach ($rpath in $resolvedPaths) {
+          if (test-path -type Container $rpath) {
+            $fsInfo = new-object System.IO.DirectoryInfo($rpath)
+          }
+          else {
+            $fsInfo = new-object System.IO.FileInfo($rpath)
+          }
+          updateFileSystemInfo($fsInfo)
+        }
+      }
+    }
+   
+    function touchNewFile([string]$path) {
+      $null > $path
+    }
+  }
+ 
+  process {
+    if ($_) {
+      if (test-path $_) {
+        touchExistingFile($_)
+      }
+      else {
+        touchNewFile($_)
+      }
+    }
+  }
+ 
+  end {
+    if ($paths) {
+      foreach ($path in $paths) {
+        if (test-path $path) {
+          touchExistingFile($path)
+        }
+        else {
+          touchNewFile($path)
+        }
+      }
+    }
+  }
+}
+
