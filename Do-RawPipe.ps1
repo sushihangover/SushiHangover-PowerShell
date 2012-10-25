87fe4e57cd602dc3e189fcabe53ab38e1f234385
sushihangover <robertnees@gmail.com>
2012-10-25 10:02:49 -0700

    Do-RawPipe, initial updates (not working as it...)
    Initial updates to orginal code, this is not working yet, continue to
    use the old code...

--- Do-RawPipe.ps1
+++ Do-RawPipe.ps1
@@ -1,95 +1,112 @@
-﻿Function Do-RawPipe {
+﻿Set-StrictMode –Version latest
+Function Pipe-Raw
+{
     <#
     .NOTES
         Copyright 2012,2013 Robert Nees
         Licensed under the Apache License, Version 2.0 (the "License");
         http://sushihangover.blogspot.com
-     .SYNOPSIS
-        Gets file encoding.
+    .SYNOPSIS
     .DESCRIPTION
         Get the standard output of a process so we can 'pipe' it
-        to another process and avoid PowerShell's object pipe
-    .EXAMPLE
-        RawPipe.ps1 makepretty.exe "-if myface.jpg -of -" findmate.exe "-if - -of listofladys.csv"
+        to another process and avoid PowerShell's object pipe which is not 
+        what you want when you do things like this in CMD.exe or a Linux shell:
         Bash >> makepretty -if myface.jpg -of - | findmate -if - -of listofladys.csv
+        Becomes in PowerShell:
+        Do-RawPipe makepretty.exe "-if myface.jpg -of -" findmate.exe "-if - -of listofladys.csv"
+    .EXAMPLE
+    dir c:\ /s /b | find "LOG" 
+    .EXAMPLE
+    dir | sort
+    cat apple.txt | wc 
+    cat apple.txt | sed -e "s/e/J/g"
+
+    .LINK
+        http://sushihangover.blogspot.com
     #>
+    [CmdletBinding()]
     param(
-        [string] $procName1, 
-        [string] $argList1, 
-        [string] $procName2, 
-        [string] $argList2, 
-        [string] $working1Path, 
-        [string] $working2Path
-    ) 
+        [Parameter(Mandatory=$True)][string] $Process1, 
+        [Parameter(Mandatory=$False)][string] $Args1, 
+        [Parameter(Mandatory=$True)][string] $Process2, 
+        [Parameter(Mandatory=$False)][string] $Args2, 
+        [Parameter(Mandatory=$False)][string] $Working1Path,
+        [Parameter(Mandatory=$False)][string] $Working2Path,
+        [Parameter(Mandatory=$False)][int] $BufferSize = 32768
+    )
+    write-output "Building Process 1 startup information"
     $process1StartInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $process1StartInfo.FileName = $procName1
+    $process1StartInfo.FileName = $Process1
     if ($working1Path -gt "") {
-        $process1StartInfo.WorkingDirectory = $working1Path
+        $process1StartInfo.WorkingDirectory = $Working1Path
     } else {
         $process1StartInfo.WorkingDirectory = ((Get-Location).Path)
     }
-    if ($argList1) {
-        $process1StartInfo.Arguments = $argList1
+    if ($Args1) {
+        $process1StartInfo.Arguments = $Args1
     }
+    write-output "Building Process 2 startup information"
     $process2StartInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $process2StartInfo.FileName = $procName2
-        if ($working2Path -gt "") {
-        $process2StartInfo.WorkingDirectory = $working2Path
+    $process2StartInfo.FileName = $Process2
+    if ($Working2Path -gt "") {
+        $process2StartInfo.WorkingDirectory = $Working2Path
     } else {
         $process2StartInfo.WorkingDirectory = ((Get-Location).Path)
     }
-    if ($argList2) { 
-        $process2StartInfo.Arguments = $argList2
+    if ($Args2) { 
+        $process2StartInfo.Arguments = $Args2
     }
 
     try {
-        $process1StartInfo.UseShellExecute = $false
-        $process1StartInfo.RedirectStandardInput = $false
-        $process1StartInfo.RedirectStandardOutput = $true
+        $process1StartInfo.UseShellExecute = $True
+        $process1StartInfo.RedirectStandardInput = $False
+        $process1StartInfo.RedirectStandardOutput = $True
    
         $process2StartInfo.UseShellExecute = $false
         $process2StartInfo.RedirectStandardInput = $true
         $process2StartInfo.RedirectStandardOutput = $false
 
-        $process1 = [System.Diagnostics.Process]::Start($process1StartInfo)
-        Write-Debug "Created $procName1 process"
-        $process2 = [System.Diagnostics.Process]::Start($process2StartInfo)
-        Write-Debug "Created $procName2 process"
+        write-output "Starting $Process1 process"
+        $process1StartInfo
+        $Process1ID = [System.Diagnostics.Process]::Start($process1StartInfo)
+        write-output "Starting $Process2 process"
+        $process2ID = [System.Diagnostics.Process]::Start($process2StartInfo)
 
         $foo = 0
-        $buffer = new-object char[] 32768
+        $buffer = new-object char[] $BufferSize
         try {
             do {
-                $readCount = $process1.StandardOutput.ReadBLock($buffer, 0, $buffer.Length)
-                $process2.StandardInput.Write($buffer, 0, $readCount)
-                $process2.StandardInput.Flush()
+                $readCount = $Process1ID.StandardOutput.ReadBLock($buffer, 0, $buffer.Length)
+                $process2ID.StandardInput.Write($buffer, 0, $readCount)
+                $process2ID.StandardInput.Flush()
                 $foo++
             } while ($readCount -gt 0)
         } catch {
-            write-debug "ReadBlock Error @ " ((($foo - 1) * $buffer.Length) + $readCount)
+            write-host "ReadBlock Error @ " ((($foo - 1) * $buffer.Length) + $readCount)
         }
-        write-verbose "$procName1 Std Output Length : " ((($foo) * $buffer.Length) + $readCount)
+        write-host "$Process1 Std Output Length : " ((($foo) * $buffer.Length) + $readCount)
         
         Write-Debug "Process2 StdIn close"
         $process2.StandardInput.Close()
         Write-Debug "Process2 wait for exit"
         #$process2.WaitForExit()
         if ($process2.HasExited) {
-            write-debug $procName2 " Exit Code = " $processs2.ExitCode
+            write-debug $Proces2 " Exit Code = " $Processs2ID.ExitCode
         }
         Write-Debug "Process1 StdOut close"
         $process1.StandardOutput.Close()
         Write-Debug "Process1 wait for exit"
         #$process1.WaitForExit()
         if ($process1.HasExited) {
-            Write-Debug $procName1 " Exit Code = " $processs1.ExitCode
+            write-debug $Process1 " Exit Code = " $Processs1ID.ExitCode
         }
     } catch {
-        Write-Debug "Catch Error and clean up"
-       #$process2.StandardInput.Flush()
+       write-host "Catch Error and clean up"
+       # $process2.StandardInput.Flush()
        #$process2.StandardInput.Close()
        #$process2.Close; $process2.Dispose()
        #$process1.StandardInput.Close()
        #$process1.Close; $process1.Dispose()
+#       exit
     }
-}
+} 
