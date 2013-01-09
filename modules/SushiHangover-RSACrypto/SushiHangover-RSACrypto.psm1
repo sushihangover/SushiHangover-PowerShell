7307a1db142802f5e1f7f49ced00e5e0e12202ef
sushihangover <robertnees@gmail.com>
2013-01-08 19:30:26 -0800

    Update RSA Crypto Routines
    Changed 'verbs' to use standard data verbs
    Moved into module directory
    Add Unittest (more of an expanded sample)
    Added Get-RSAClearText, that combines the functions Get-RSAClearBytes
    and Convert-ByteToString to simplify usage (based upon external
    comments)

---
+++ modules/SushiHangover-RSACrypto/SushiHangover-RSACrypto.psm1
@@ -0,0 +1,155 @@
+﻿Set-StrictMode –Version latest
+<#
+.NOTES
+    Copyright 2012/2013 Robert Nees
+    Licensed under the Apache License, Version 2.0 (the "License");
+    http://sushihangover.blogspot.com
+.SYNOPSIS
+    RSA Private Key encrypt and decrypt functions
+.DESCRIPTION
+    This is a collection of functions that provide a wrapper to 'opensslkey.cs'
+
+    Note: The source code in opensslkey.cs: 
+        Copyright (C) 2008 JavaScience Consulting (See header for full details)
+        I did make edits to the C# code but hold no copyright on it.
+        
+        The opesslkey.cs works via Add-Type with .Net 4.5 under Windows 8 / PowerShell 3.0 
+        without any issues that I have seen.
+.EXAMPLE
+    Import-Module SushiHangover-RSACrypnto
+    $PemFile = $HOME + '\.ssh\SSH2RSA2048_private_test_key.pem'
+    . Set-RSACryptoServiceProvider $PemFile
+    $preclearText = "Hello, Lets keep this a secret, OK?"
+    Write-Host 'Clear Text of:'
+    Write-Host '--------------'
+    Write-Host $preclearText
+    Write-Host '------------------------------------------------'
+    Write-Host 'Becomes Encrypted and viewed as a Base64 String:'
+    Write-Host '------------------------------------------------'
+    $clearBytes =  Convert-StringToByte $preclearText
+    $clearBase64 = ConvertTo-Base64String $clearBytes
+    $cypherBytes = Get-RSAEncryptedBytes $clearBase64 $false
+    $cypherBase64  = ConvertTo-Base64String $cypherBytes
+    Write-Host 'Encrypted Base64 String:'
+    Write-Host '------------------------'
+    $cypherBase64
+    # The multiple step way using byte output
+    $clearBytes = Get-RSAClearBytes $cypherBase64 $false
+    $postclearText =  Convert-ByteToString $clearBytes
+    Compare-Object $preclearText $postclearText -CaseSensitive -IncludeEqual
+    # Or convert directly to clear text
+    $postclearText = Get-RSAClearText $cypherBase64 $false
+    Compare-Object $preclearText $postclearText -CaseSensitive -IncludeEqual
+    Remove-Module SushiHangover-RSACrypto
+.LINK
+    http://sushihangover.blogspot.com
+    http://ruudvanderlinden.com/2010/10/19/running-inline-c-with-custom-assemblies-in-powershell-2-0/
+    http://blogs.technet.com/b/stefan_gossner/archive/2010/05/07/using-csharp-c-code-in-powershell-scripts.aspx
+    http://msdn.microsoft.com/en-us/library/system.security.cryptography.rsacryptoserviceprovider.aspx
+#>
+$CSFile = Join-Path -Path $PSScriptRoot -ChildPath 'opensslkey.cs’
+$SourceCode = [System.IO.File]::ReadAllText($CSFile)
+$Assemblies = ('System','System.Security')
+
+try {
+    Add-Type -TypeDefinition $SourceCode -ReferencedAssemblies $Assemblies -IgnoreWarnings -Language CSharp 
+} catch {
+    Write-Warning "An error occurred attempting to add the .NET Framework class to the PowerShell session."
+    Write-Warning "The error was: $($Error[0].Exception.Message)"
+}
+Function ConvertTo-Base64String {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('pem')][array]$ByteString
+    )
+    return [System.Convert]::ToBase64String($ByteString)
+}
+Function ConvertFrom-Base64String {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('pem')][string]$ByteString
+    )
+    return [System.Convert]::FromBase64String($ByteString)
+}
+Function Convert-ByteToString {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('byte')][byte[]]$AsciiByteArray
+    )
+    return [System.Text.Encoding]::ASCII.GetString($AsciiByteArray)
+}
+Function Convert-StringToByte {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('byte')][string]$AsciiByteString
+    )
+    return [System.Text.Encoding]::ASCII.GetBytes($AsciiByteString)
+}
+Function Set-RSACryptoServiceProvider {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('pem')][Alias('p')][string]$PemPrivateKeyFile
+    )
+    if (Test-Path -Path $PemPrivateKeyFile) {
+        Write-Debug "Pem file exists"
+        $PemText = [System.IO.File]::ReadAllText($PemPrivateKeyFile)
+        Write-Debug $PemText
+        $PemPrivateKey = [javascience.opensslkey]::DecodeOpenSSLPrivateKey($PemText)
+        [System.Security.Cryptography.RSACryptoServiceProvider]$RSA = [javascience.opensslkey]::DecodeRSAPrivateKey($PemPrivateKey);
+        Write-Debug $RSA
+    } else {
+        Write-Debug "PEM File does not exist"
+        $RSA = $null
+    }
+    new-variable -scope global -name RSACryptoServiceProvider -force 
+    $RSACryptoServiceProvider = $RSA
+}
+Function Get-RSACryptoServiceProvider {
+    if (!$RSACryptoServiceProvider) {
+        . Set-RSACryptoServiceProvider
+    }
+    return $RSACryptoServiceProvider
+}
+Function Get-RSAClearBytes {
+    <#
+    .SYNOPSIS
+        RSA Decryption to Bytes with optional OAEP padding
+    .Parameter OAEPPadding
+        True to perform direct RSA decryption using OAEP padding (only available on a computer
+        running Microsoft Windows XP or later); otherwise, false to use PKCS#1 v1.5 padding.
+    .Parameter out
+        Returns Decrypted Base64 String
+    #>
+    Param (
+        [Parameter(Mandatory=$true)][Alias('cypher')][string]$CypherBase64,
+        [Parameter(Mandatory=$true)][Alias('oaep')][boolean]$OAEPPadding = $false,
+        [Parameter(Mandatory=$false)][Alias('RSA')][System.Security.Cryptography.RSACryptoServiceProvider]$RSACryptoServiceProvider = (. Get-RSACryptoServiceProvider)
+    )
+    $CypherBytes  = [System.Convert]::FromBase64String($CypherBase64)
+    $ClearBytes = $RSACryptoServiceProvider.Decrypt($CypherBytes, $OAEPPadding)
+    Return $ClearBytes
+}
+Function Get-RSAClearText {
+    <#
+    .SYNOPSIS
+        RSA Decryption to ClearText with optional OAEP padding
+    .Parameter OAEPPadding
+        True to perform direct RSA decryption using OAEP padding (only available on a computer
+        running Microsoft Windows XP or later); otherwise, false to use PKCS#1 v1.5 padding.
+    .Parameter out
+        Returns Decrypted "Clear Text" String
+    #>
+    Param (
+        [Parameter(Mandatory=$true)][Alias('cypher')][string]$CypherBase64,
+        [Parameter(Mandatory=$true)][Alias('oaep')][boolean]$OAEPPadding = $false,
+        [Parameter(Mandatory=$false)][Alias('RSA')][System.Security.Cryptography.RSACryptoServiceProvider]$RSACryptoServiceProvider = (. Get-RSACryptoServiceProvider)
+    )
+    $clearBytes = Get-RSAClearBytes $CypherBase64 $OAEPPadding
+    Return Convert-ByteToString $clearBytes
+}
+Function Get-RSAEncryptedBytes {
+    Param (
+        [Parameter(Mandatory=$true)][Alias('cypher')][array]$ClearBase64,
+        [Parameter(Mandatory=$true)][Alias('oaep')][boolean]$OAEPPadding = $false,
+        [Parameter(Mandatory=$false)][Alias('RSA')][System.Security.Cryptography.RSACryptoServiceProvider]$RSACryptoServiceProvider = (. Get-RSACryptoServiceProvider)
+    )
+    $ClearBytes  = [System.Convert]::FromBase64String($ClearBase64)
+    $CypherBytes = $RSACryptoServiceProvider.Encrypt($ClearBytes, $OAEPPadding)
+    #$CypherString = [System.Text.Encoding]::ASCII.GetString($CypherBytes)
+    Return $CypherBytes
+}
---
+++ modules/SushiHangover-RSACrypto/UnitTest-SushiHangover.ps1
@@ -0,0 +1,44 @@
+﻿Set-StrictMode –Version latest
+<#
+.NOTES
+    Copyright 2013 Robert Nees
+    Licensed under the Apache License, Version 2.0 (the "License");
+    http://sushihangover.blogspot.com
+.SYNOPSIS
+    Test the SushiHangover-RSACrypto module
+.DESCRIPTION
+    This is a collection of functions to test the SushiHangover-RSACrypto module
+
+    It is not much of a unit test, but more like an example. 
+    
+    If the Compare-Object output is different than the module is 'broken', please report 
+    any problems via SushiHangover on GitHub and/or http://sushihangover.blogspot.com
+.EXAMPLE
+    UnitTest-SushiHangover.ps1
+.LINK
+    http://sushihangover.blogspot.com
+#>
+Import-Module SushiHangover-RSACrypto
+$PemFile = $HOME + '\.ssh\SSH2RSA2048_private_test_key.pem'
+. Set-RSACryptoServiceProvider $PemFile
+$preclearText = "Hello, Lets keep this a secret, OK?"
+Write-Host 'Clear Text of:' 
+Write-Host '--------------'
+Write-Host $preclearText -ForegroundColor Green
+Write-Host
+$preclearBytes =  Convert-StringToByte $preclearText
+$clearBase64 = ConvertTo-Base64String $preclearBytes
+$cypherBytes = Get-RSAEncryptedBytes $clearBase64 $false
+$cypherBase64  = ConvertTo-Base64String $cypherBytes
+Write-Host 'Encrypted Base64 String:'
+Write-Host '------------------------'
+Write-Host $cypherBase64  -ForegroundColor Red
+Write-Host
+Write-Host 'Compare-Object output using input bytes via Get-RSAClearBytes:'
+$postclearBytes = Get-RSAClearBytes $cypherBase64 $false
+Compare-Object $preclearBytes $postclearBytes -CaseSensitive -IncludeEqual |Format-Table -AutoSize
+Write-Host 'Compare-Object output using clear text input via Get-RSAClearText:'
+$postclearText = Get-RSAClearText $cypherBase64 $false
+Write-Host
+Compare-Object $preclearText $postclearText -CaseSensitive -IncludeEqual
+Remove-Module SushiHangover-RSACrypto
