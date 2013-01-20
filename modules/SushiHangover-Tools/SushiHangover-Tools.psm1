873283ff2de9160b322420973a7cae88da718af1
sushihangover <robertnees@gmail.com>
2013-01-19 19:02:18 -0800

    Script module for SushiHangover-Tools

---
+++ modules/SushiHangover-Tools/SushiHangover-Tools.psm1
@@ -0,0 +1,14 @@
+﻿<#
+.NOTES
+    Copyright 2013 Robert Nees
+    Licensed under the Apache License, Version 2.0 (the "License");
+    http://sushihangover.blogspot.com
+.SYNOPSIS
+    Script module for SushiHangover-Tools
+.DESCRIPTION
+    Script module for SushiHangover-Tools
+.LINK
+    http://sushihangover.blogspot.com
+.LINK
+    https://github.com/sushihangover
+#>
