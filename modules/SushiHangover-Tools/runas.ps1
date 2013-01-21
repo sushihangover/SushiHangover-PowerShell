6f9db405397b241fe15ca3d9a3e6d537fab3a756
sushihangover <robertnees@gmail.com>
2013-01-20 17:26:56 -0800

    Removed bad commits

--- modules/SushiHangover-Tools/runas.ps1
+++
@@ -1,7 +0,0 @@
-﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-{   
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-Break
-}
